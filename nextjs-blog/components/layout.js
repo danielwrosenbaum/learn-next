b9625929bbfab1d6910921dbccdf8efad274ad34
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-29 00:32:09 +0000

    installed tailwind and added config files for tailwind and postcss. updated home to show data mapped from posts directory. #1

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Daniel Rosenbaum'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Daniel's Sample Website"
 
 export default function Layout({ children, home }) {
   return (
